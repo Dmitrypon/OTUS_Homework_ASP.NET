db99308b56480bc6263da8195a5b33de8cf044c7
Dmitry Ponomar <mitrybox@gmail.com>
2024-11-29 13:23:58 +0300

    WebHost RolesController changed IRepository with Mapper

--- UnitTests/src/PromoCodeFactory.WebHost/Controllers/RolesController.cs
+++ UnitTests/src/PromoCodeFactory.WebHost/Controllers/RolesController.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using PromoCodeFactory.Core.Abstractions.Repositories;
 using PromoCodeFactory.Core.Domain.Administration;
 using PromoCodeFactory.WebHost.Models;
+using PromoCodeFactory.WebHost.Models.Responses;
 
 namespace PromoCodeFactory.WebHost.Controllers
 {
@@ -13,33 +18,21 @@ namespace PromoCodeFactory.WebHost.Controllers
     /// </summary>
     [ApiController]
     [Route("api/v1/[controller]")]
-    public class RolesController
+    public class RolesController(IRepository<Role, Guid> rolesRepository, IMapper mapper)
     {
-        private readonly IRepository<Role> _rolesRepository;
-
-        public RolesController(IRepository<Role> rolesRepository)
-        {
-            _rolesRepository = rolesRepository;
-        }
-        
         /// <summary>
         /// Получить все доступные роли сотрудников
-        /// </summary>
+        /// Get all available Employee roles
+        /// </summary> 
         /// <returns></returns>
         [HttpGet]
-        public async Task<IEnumerable<RoleItemResponse>> GetRolesAsync()
+        [ProducesResponseType(typeof(IEnumerable<RoleItemResponse>), 200)]
+        public async Task<IEnumerable<RoleItemResponse>> GetRoleAsync()
         {
-            var roles = await _rolesRepository.GetAllAsync();
-
-            var rolesModelList = roles.Select(x => 
-                new RoleItemResponse()
-                {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Description = x.Description
-                }).ToList();
-
+            var roles = await rolesRepository.GetAllAsync();
+            var rolesModelList = roles.Select(mapper.Map<RoleItemResponse>).ToList();
             return rolesModelList;
         }
+       
     }
 }
