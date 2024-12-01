7e39f8e82f89248e4f6a6b536a9ef21e44f25a5c
Dmitry Ponomar <mitrybox@gmail.com>
2024-12-01 20:16:35 +0300

    EF IRepository is changed, Base with IEntity added

--- EF/src/PromoCodeFactory.Core/Domain/Administration/Employee.cs
+++ EF/src/PromoCodeFactory.Core/Domain/Administration/Employee.cs
@@ -1,21 +1,20 @@
-﻿using PromoCodeFactory.Core.Domain;
-using System;
+﻿using System;
+using System.Collections;
+using PromoCodeFactory.Core.Domain;
 using System.Collections.Generic;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 
 namespace PromoCodeFactory.Core.Domain.Administration
 {
-    public class Employee
-        : BaseEntity
+    public class Employee : BaseEntity
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
         public string FullName => $"{FirstName} {LastName}";
-
         public string Email { get; set; }
-
-        public Role Role { get; set; }
-
+        public Guid RoleId { get; set }
+        public virtual Role Role { get; set; }
         public int AppliedPromocodesCount { get; set; }
+        public virtual IEnumerable<PromoCode> PromoCodes { get; set; }        
     }
 }
--- EF/src/PromoCodeFactory.Core/Domain/Administration/Role.cs
+++ EF/src/PromoCodeFactory.Core/Domain/Administration/Role.cs
@@ -1,13 +1,13 @@
 ﻿using PromoCodeFactory.Core.Domain;
 using System;
+using System.Collections;
 
 namespace PromoCodeFactory.Core.Domain.Administration
 {
-    public class Role
-        : BaseEntity
+    public class Role : BaseEntity
     {
         public string Name { get; set; }
-
         public string Description { get; set; }
+        public virtual IEnumerable<Employee> Employees { get; set; }
     }
 }
