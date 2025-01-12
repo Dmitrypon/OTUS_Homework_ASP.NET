bbb199e22b083a8f44004d19cbcf9253382f74ea
Dmitry Ponomar <mitrybox@gmail.com>
2025-01-12 21:04:36 +0300

    MessageDTO added

---
+++ RabbitMQ/src/Pcf.Administration/Pcf.Administration.WebHost/Consumers/NotifyAppliedPromocodeConsumer.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Domain.Administration;
+using Pcf.Administration.WebHost.Contracts;
+using Pcf.Administration.WebHost.Models;
+using Pcf.Administration.WebHost.Contracts;
+using Pcf.Contracts;
+using System.Threading.Tasks;
+
+namespace Pcf.Administration.WebHost.Consumers
+{
+    public class NotifyAppliedPromocodeConsumer(
+        IRepository<Employee> employeeRepository
+        , ILogger<NotifyAppliedPromocodeConsumer> logger) : IConsumer<MessageDTO>
+    {
+        async Task IConsumer<MessageDto>.Consume(ConsumeContext<MessageDto> context)
+        {
+            var uid = context.Message.Uid;
+            var employee = await employeeRepository.GetByIdAsync(uid);
+
+            if (employee == null)
+            {
+                logger.LogInformation($"User by guid = {uid} not found");
+                return;
+            }
+
+            employee.AppliedPromocodesCount++;
+
+            await employeeRepository.UpdateAsync(employee);
+        }
+    }
+}
---
+++ RabbitMQ/src/Pcf.Administration/Pcf.Administration.WebHost/Contracts/MessageDTO.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Pcf.Administration.Core.Abstractions.Repositories;
+using Pcf.Administration.Core.Domain.Administration;
+using Pcf.Administration.WebHost.Contracts;
+using Pcf.Contracts;
+using System.Threading.Tasks;
+
+
+namespace Pcf.Administration.WebHost.Contracts;
+
+public class MessageDTO
+{	
+         public Guid Uid { get; set; }
+}
+
