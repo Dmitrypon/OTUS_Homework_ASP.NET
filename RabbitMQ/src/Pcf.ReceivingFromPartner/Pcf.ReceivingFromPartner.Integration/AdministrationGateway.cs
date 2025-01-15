2555a9107c483eab9e7126d0e49c55767b9d7319
Dmitry Ponomar <mitrybox@gmail.com>
2025-01-15 21:21:54 +0300

    Pcf.ReceivingFromPartner.Integration AdministrationGateway changed

--- RabbitMQ/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/AdministrationGateway.cs
+++ RabbitMQ/src/Pcf.ReceivingFromPartner/Pcf.ReceivingFromPartner.Integration/AdministrationGateway.cs
@@ -2,25 +2,44 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Pcf.ReceivingFromPartner.Core.Abstractions.Gateways;
+using MassTransit;
+using Pcf.Contracts;
 
 namespace Pcf.ReceivingFromPartner.Integration
 {
     public class AdministrationGateway
-        : IAdministrationGateway
+      : IAdministrationGateway
     {
-        private readonly HttpClient _httpClient;
+        private readonly IBusControl _busControl;
 
-        public AdministrationGateway(HttpClient httpClient)
+        public AdministrationGateway(
+            IBusControl busControl
+            )
         {
-            _httpClient = httpClient;
+            _busControl = busControl;
         }
 
         public async Task NotifyAdminAboutPartnerManagerPromoCode(Guid partnerManagerId)
         {
-            var response = await _httpClient.PostAsync($"api/v1/employees/{partnerManagerId}/appliedPromocodes",
-                new StringContent(string.Empty));
-
-            response.EnsureSuccessStatusCode();
+            await _busControl.Publish(new PromoCodeMessageDTO { Id = partnerManagerId });
         }
     }
+    //public class AdministrationGateway
+    //    : IAdministrationGateway
+    //{
+    //    private readonly HttpClient _httpClient;
+
+    //    public AdministrationGateway(HttpClient httpClient)
+    //    {
+    //        _httpClient = httpClient;
+    //    }
+
+    //    public async Task NotifyAdminAboutPartnerManagerPromoCode(Guid partnerManagerId)
+    //    {
+    //        var response = await _httpClient.PostAsync($"api/v1/employees/{partnerManagerId}/appliedPromocodes",
+    //            new StringContent(string.Empty));
+
+    //        response.EnsureSuccessStatusCode();
+    //    }
+    //}
 }
